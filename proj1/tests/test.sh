d9c9e3ba3851178ce87ec1ab9cd14359300a06c3
Patrick Fernandes <pattuga@gmail.com>
2016-03-12 00:11:42 +0000

    Fix variable

--- proj1/tests/test.sh
+++ proj1/tests/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Tests the program with input/outpu files in the same folder
 
-program="build/proj1"
+program="../build/proj1"
 
 for inputfile in *.in; do
-	diff <(eval ../$program <$inputfile) ${inputfile%.*}.out
+	diff <(eval $program <$inputfile) ${inputfile%.*}.out
 done
 
 # @copyright Grupo 70
