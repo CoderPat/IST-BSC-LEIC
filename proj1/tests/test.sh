98a0f09fa63c8e22c50abf80006f678c94f6ffcf
Patrick Fernandes <pattuga@gmail.com>
2016-03-12 00:09:59 +0000

    Improve test script

--- proj1/tests/test.sh
+++ proj1/tests/test.sh
@@ -1,5 +1,10 @@
-directory="/build/"
+#!/bin/bash
+#Tests the program with input/outpu files in the same folder
+
+program="build/proj1"
 
 for inputfile in *.in; do
-	diff <(../${directory}proj1 <$inputfile) ${inputfile%.*}.out
+	diff <(eval ../$program <$inputfile) ${inputfile%.*}.out
 done
+
+# @copyright Grupo 70
