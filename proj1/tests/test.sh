417412a66f13b23ef597f1b0214036ca9ad3fb17
Patrick Fernandes <pattuga@gmail.com>
2016-03-16 22:17:50 +0000

    Improve testing script

--- proj1/tests/test.sh
+++ proj1/tests/test.sh
@@ -2,10 +2,30 @@
 #Tests the program with input/outpu files in the same folder
 
 program="../build/proj1"
+max_vertices=10000
+
 
 for inputfile in *.in; do
 	echo -------$inputfile--------
 	diff <(eval $program <$inputfile) ${inputfile%.*}.out
 done
+
+#--- Performance mesure ---
+if [ "$1" == "-perf" ]; then
+	echo "#### Testing Performance ####"
+	total_test="$2"
+	echo "---- Generating Test Cases ----"
+	while (( total_test > 0 )); do
+		python randomgraphgenerator.py $max_vertices >t${total_test}.random.in 
+		((total_test--))
+	done
+
+	for inputfile in *.random.in; do
+		perf stat -x, -e instructions:u $program < $inputfile >/dev/null
+	done
+
+	rm *.random.in
+fi
+
 
 # @copyright Grupo 70
