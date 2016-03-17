a42718f6216d01cb60e0703ed97dcce521fa5e38
CoderPat <pattuga@gmail.com>
2016-03-17 01:09:07 +0000

    Finish test script

--- proj1/tests/randomgraphgenerator.py
+++ proj1/tests/randomgraphgenerator.py
@@ -7,16 +7,19 @@ if __name__ == "__main__":
 	if len(sys.argv) < 2:
 		print "Usage <max_vertices>"
 
+	#Generate random graph (probability of edge generation calculated so the graph is sparse)
 	num_vertices = random(int(sys.argv[1]))
 	graph = nx.fast_gnp_random_graph(num_vertices, p=1.0/float(num_vertices/2), directed=False)
 
+	#If the graph is not connected, connect the the connected subgrafs
 	if not nx.is_connected(graph):
 		comps = list(nx.connected_components(graph))
 		for i in xrange(len(comps)-1):
 			graph.add_edge(choice(list(comps[i])), choice(list(comps[i+1])))
 
-	assert nx.is_connected(graph), "something went wrong"
+	assert nx.is_connected(graph), "Y U DO THIS?"
 
+	#Print to output format
 	edges = nx.to_edgelist(graph)
 	print "%s %s" % (num_vertices, len(edges))
 	for v1, v2, _ in edges:
--- proj1/tests/test.sh
+++ proj1/tests/test.sh
@@ -2,7 +2,8 @@
 #Tests the program with input/outpu files in the same folder
 
 program="../build/proj1"
-max_vertices=10000
+perf_file="perf_results.dat"
+max_vertices=100000
 
 
 for inputfile in *.in; do
@@ -15,13 +16,21 @@ if [ "$1" == "-perf" ]; then
 	echo "#### Testing Performance ####"
 	total_test="$2"
 	echo "---- Generating Test Cases ----"
+	# generate random graphs
 	while (( total_test > 0 )); do
 		python randomgraphgenerator.py $max_vertices >t${total_test}.random.in 
 		((total_test--))
 	done
 
+	echo "---- Checking Runtime ----"
 	for inputfile in *.random.in; do
-		perf stat -x, -e instructions:u $program < $inputfile >/dev/null
+		# test instructions used
+		instr=$(perf stat -x, -e instructions:u $program < $inputfile 2>&1 >/dev/null)
+
+		#prints .dat file with the V+E <-> Intructions correspondence
+		graph_info=$(head -n 1 $inputfile)
+		graph_info_array=($graph_info)
+		echo $((${graph_info_array// /+})) ${instr%,,*} >> $perf_file
 	done
 
 	rm *.random.in
