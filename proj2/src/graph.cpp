4351810db97884d7ee3387f7da7b1c0fc7d47306
Patrick Fernandes <pattuga@gmail.com>
2016-04-17 21:36:53 +0100

    Finish all pairs shortest distance algorithm

--- proj2/src/graph.cpp
+++ proj2/src/graph.cpp
@@ -22,19 +22,15 @@ void Graph::add_edge(int vertex1, int vertex2, int weight){
 
 
 void Graph::__bellman_ford_reweight(std::vector<int>& distances){
-    for(int i = 0; i < _number_of_vertices; i++) {
-      distances[i] = std::numeric_limits::infinity();
-    } 
+
     for(int i = 0; i < _number_of_vertices; i++)
-        if(distance[vertex2] > 0)
-            distance[vertex2] = 0;
-    }
-    for(int i = _0; i < _number_of_vertices; i++){
-      for(int j = 0; j < _number_of_vertices; j++)
-        for(std::list<int>::iterator vertex_it = _graph_lists[j].begin(); vertex_it != _graph_lists[*vertex_it].end(); vertex_it++)
-          if(distance[vertex2] > distance[vertex1] + _edges_weights[vertex1][vertex2])
-             distance[vertex2] = distance[vertex1] + _edges_weights[vertex1][vertex2];
-    } 
+        distances[i] = 0;
+    
+    for(int i = 0; i < _number_of_vertices; i++)
+        for(int j = 0; j < _number_of_vertices; j++)
+            for(std::list<int>::iterator vertex_it = _graph_lists[j].begin(); vertex_it != _graph_lists[j].end(); vertex_it++)
+                if(distances[*vertex_it] > distances[j] + _edges_weights[j][*vertex_it])
+                    distances[*vertex_it] = distances[j] + _edges_weights[j][*vertex_it];
 }
 
 void Graph::__dijkstras(int source_vertex, 
@@ -69,17 +65,22 @@ void Graph::__dijkstras(int source_vertex,
 void Graph::find_shortest_paths(const std::vector<int>& origins, 
                                 std::vector<std::vector<int> >& distances){
 
-    std::vector<int> bell_distances;
+    std::vector<int> bell_distances(_number_of_vertices, 0);
     std::vector<std::vector<int> > positive_weigths(_number_of_vertices, std::vector<int>(_number_of_vertices, 0) );
-    /*
-    __bellman_ford(bell_distances);
+    
+    __bellman_ford_reweight(bell_distances);
 
-     for(int vertex = 0; vertex < _number_of_vertices; vertex++)
+    
+    for(int vertex = 0; vertex < _number_of_vertices; vertex++)
         for(std::list<int>::iterator adj_vertex_it = _graph_lists[vertex].begin(); adj_vertex_it != _graph_lists[vertex].end(); adj_vertex_it++)
             positive_weigths[vertex][*adj_vertex_it] = _edges_weights[vertex][*adj_vertex_it] + 
                                                        bell_distances[vertex] - bell_distances[*adj_vertex_it];
 
-    */
+    for(int i = 0 ; i < (int) origins.size(); i++){
+        __dijkstras(origins[i]-1, positive_weigths, distances[i]);
+        for(int j=0; j < _number_of_vertices; j++)
+            distances[i][j] += bell_distances[j] - bell_distances[origins[i]-1];;
+    }
     
 }
 
