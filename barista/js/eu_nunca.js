ddc7d60865f865af0f99f5548785a171e511b3a7
CoderPat <pattuga@gmail.com>
2016-04-27 22:10:18 +0100

    Improve NHIE game

--- barista/js/eu_nunca.js
+++ barista/js/eu_nunca.js
@@ -3,9 +3,53 @@ var questions = [["Eu nunca precisei de um advogado", 0, 0 ],
 				 ["Eu nunca fui revistado pela policia", 0, 0]];
 
 function get_random_question(){
-	return questions[Math.floor(Math.random()*questions.length)];
+    var i =  (Math.floor(Math.random()*questions.length) + 1) % questions.length;
+    return questions[i];
 }
 
 $(document).ready(function() {
 	$('.question h3').text(get_random_question()[0]);
+
+    $('#next_button').click(function(){
+        $('.question h3').text(get_random_question()[0]);
+    });
+
+    $('#already_button').click(function(){
+        for(var i = 0; i < questions.length; i++)
+            if($('.question h3').text() === questions[i][0])
+                questions[i][1]++;
+    });
+
+    $('#never_button').click(function(){
+        for(var i = 0; i < questions.length; i++)
+            if($('.question h3').text() === questions[i][0])
+                questions[i][2]++;
+    });
+
+    $('#highscore_button').click(function(){
+
+        item('cart');
+
+        questions.sort(function(question_info1, question_info2){
+            var p_never_1 = question_info1[2] === 0 ? 0 : question_info1[2]/(question_info1[1] + question_info1[2]);
+            var p_never_2 = question_info2[2] === 0 ? 0 : question_info2[2]/(question_info2[1] + question_info2[2]);
+            console.log('HELLO');
+            return p_never_2 - p_never_1;
+        });
+
+        $('.perguntas_div').empty();
+        for(var i = 0; i < questions.length; i++){
+            if (questions[i][2] != 0)
+                $('.perguntas_div').append(questions[i][0]+'<br>' + questions[i][2] + '<br>');
+        }
+    })
 });
+
+// When a user cliks on a item
+function item(id) {
+  id = document.getElementById(id);
+  if(id.style.display == 'block')
+     id.style.display = 'none';
+  else
+     id.style.display = 'block';
+}
