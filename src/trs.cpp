71bb7a7f2bafe1aeec969a039c0b052b0c905337
CoderPat <pattuga@gmail.com>
2016-09-26 17:34:47 +0100

    Add trs class draft

--- src/trs.cpp
+++ src/trs.cpp
@@ -1,6 +1,11 @@
 #include <tcplib.hpp>
 #include <udplib.hpp>
 
+class TRSInterface{
+private:
+    TCPServer server
+}
+
 int main(int argc, char **argv) {
     ushort_t trs_port = 59000 + GN;
     ushort_t tcs_port = 58000 + GN; 
@@ -14,13 +19,13 @@ int main(int argc, char **argv) {
     for (int i = 2; i<argc-1; i++) {
         if (!strcmp("-p", argv[i])){
             i++;
-
+            trs_port = stoi(argv[i])
         } else if (!strcmp("-n", argv[i])) {
             i++;
             hostname = string(argv[i]);
         } else if (!strcmp("-e", argv[i])) {
             i++;
-            hostname = string(argv[i]);
+            tcs_port = stoi(argv[i]);
         }
         cerr << "Unknow option: " << argv[i] << endl;
     }
