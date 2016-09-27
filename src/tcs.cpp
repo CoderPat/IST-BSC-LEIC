255fdb0e44ab19a8df63d58f04f4dcf1b6003c93
topastop <tope_lourenco@yahoo.com>
2016-09-27 16:38:10 +0100

    REFACTOR - Refactored the TCS server code in order to work with the new libs.

--- src/tcs.cpp
+++ src/tcs.cpp
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <map>
+#include <algorithm>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,72 +13,111 @@
 #include <iostream>
 #include <stdlib.h>
 #include "../include/udplib.hpp"
-/*
-UdpServer::UdpServer(int port, std::string langsfile){
-    _fd = socket(AF_INET,SOCK_DGRAM,0);
 
-    _serveraddr.sin_family=AF_INET;
-    _serveraddr.sin_addr.s_addr=htonl(INADDR_ANY);
-    _serveraddr.sin_port=htons((u_short)port);
-    _langsfile = langsfile;
-
-    bind(_fd,(struct sockaddr*)&_serveraddr, sizeof(_serveraddr));
-}
-
-std::string UdpServer::wait_message(){
-    char buffer[4096];
-
-    _clientaddrlen = sizeof(_clientaddr);
-    recvfrom(_fd,buffer,sizeof(buffer),0,(struct sockaddr*)&_clientaddr,(socklen_t*)&_clientaddrlen);
-
-    return std::string(buffer);
-}
-
-void UdpServer::answer_last(const std::string& message){
-    sendto(_fd, &message[0], message.length() + 1, 0, (struct sockaddr*)&_clientaddr, _clientaddrlen);
-}
-
-void  UdpServer::parse_avaliable_languages() {
-    std::ifstream filetoparse;
-    std::string lang;
-	filetoparse.open(_langsfile.c_str());
-	if(filetoparse.is_open()) {
-		while (std::getline(filetoparse, lang)) {
-            std::string langport = lang;
-            lang = lang.substr(0, lang.find(' ', 0));
-            _langs[lang] = atoi(langport.substr(langport.find(' ') + 1).c_str());
-            std::cout << lang << "  " << _langs[lang] << std::endl;
-		}
+class TCS : public UDPServer{
+private:
+    std::map<std::string, int> _langs;
+    std::string _langsfile = "langs.txt";
+public:
+    
+    TCS(u_short port) : UDPServer(port) { 
+        _langsfile = "langs.txt";
     }
-}
 
-std::vector<std::string>  UdpServer::get_avaliable_languages() {
-    std::vector<std::string> out;
-    for(std::map<std::string,int>::iterator it = _langs.begin(); it != _langs.end(); ++it) {
-        out.push_back(it->first);
+    std::map<std::string, int> get_langs() {
+        return _langs;
     }
-	return out;
-}
 
-UdpServer::~UdpServer(){
-    close(_fd);
-}*/
+    void  parse_avaliable_languages() {
+        std::ifstream filetoparse;
+        std::string lang;
+        filetoparse.open(_langsfile.c_str());
+        if(filetoparse.is_open()) {
+            while (std::getline(filetoparse, lang)) {
+                std::string langport = lang;
+                lang = lang.substr(0, lang.find(' ', 0));
+                _langs[lang] = atoi(langport.substr(langport.find(' ') + 1).c_str());
+                std::cout << lang << "  " << _langs[lang] << std::endl;
+            }
+        }
+    }
+
+    std::vector<std::string>  get_avaliable_languages() {
+        std::vector<std::string> out;
+        for(std::map<std::string,int>::iterator it = _langs.begin(); it != _langs.end(); ++it) {
+            out.push_back(it->first);
+        }
+        return out;
+    }
+
+    std::string uint8_tToString(std::vector<uint8_t> vec) {
+        std::string out = "";
+        for(int i = 0; i < vec.size(); i++) {
+            out += vec[i];
+        }
+        return out;
+    }
+
+    ~TCS() {
+        //close(_fd);
+    }
+
+    std::vector<std::string> tokenizer(std::string in) {
+
+        std::vector<std::string> out;
+        std::string buffer = "";
+        for(int i = 0; i < in.length(); i++) {
+            if(in[i] != ' ' && in[i] != '\n') {
+                buffer += in[i];
+            }
+            else {
+                out.push_back(buffer);
+                buffer = "";
+            }
+        }
+        std::cout << out[0] << std::endl;
+        return out;
+    }
+};
 
 int main(int argc, char* args[]) {
 
-    if(argc != 3) {
-        std::cout << "Usage: TCS <port> <langs.txt>" << std::endl;
-        return 0;
+
+    TCS server(8112);
+    server.parse_avaliable_languages();
+    server.get_avaliable_languages();
+
+    while(1) {
+        std::vector<uint8_t> msg = server.Read();
+        std::string msgstr = server.uint8_tToString(msg);
+        std::vector<std::string> input = server.tokenizer(msgstr);
+        std::vector<std::string> avlangs = server.get_avaliable_languages();
+        std::string response = "";
+
+        bool secure = (input.size() == 1 || input.size() == 2) ? true : false;
+
+        if(secure && !strcmp("ULQ", input[0].c_str())) {
+            for(int i = 0; i < avlangs.size(); i++) {
+                response = response + avlangs[i] + " ";
+            }
+            std::cout << response << std::endl;
+        }
+        else if (secure && !strcmp("UNQ", input[0].c_str())) {
+            
+            if(std::find(avlangs.begin(), avlangs.end(), input[1]) != avlangs.end())
+            {
+                response = std::to_string(server.get_langs()[input[1]]);
+            }
+            else {
+                response = "Language not supported";
+            }
+        }
+        else {
+            response = "Error";
+        }
+        if(secure) {
+            std::cout << response << std::endl;
+            server.Write(std::vector<uint8_t>(response.begin(), response.end()));
+        }
     }
-
-    UDPServer server(8090);
-    //UdpServer server(atoi(args[1]), args[2]);
-    //server.parse_avaliable_languages();
-    //server.get_avaliable_languages();
-
-    std::vector<uint8_t> msg = server.Read();
-
-    std::string a = "asd";
-    
-    server.Write(std::vector<uint8_t>(a.begin(), a.end()));
 }
