4dbf76a9204f28311c1a8a67a62d5a94487f64ea
xtrm0 <afonso.tfaria21@gmail.com>
2016-10-10 00:29:53 +0100

    Closes #8. Adds correct compilation for inline functions

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,30 +1,16 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include "signal.h"
 
 #define GN 7
 
-inline std::vector<uint8_t> byte_cast(const std::string& str){
-    return std::vector<uint8_t>(str.begin(), str.end());
-}
+std::vector<uint8_t> byte_cast(const std::string& str);
 
-inline std::string string_cast(const std::vector<uint8_t>& bytes){
-    return std::string(bytes.begin(), bytes.end());
-}
+std::string string_cast(const std::vector<uint8_t>& bytes);
 
-std::vector<std::string> tokenize(const std::string& str){
-    std::vector<std::string> tokens;
-    std::string aux;
-    std::stringstream ss(str);
-    while (ss >> aux)
-        tokens.push_back(aux);
-    return tokens;
-}
+std::vector<std::string> tokenize(const std::string& str);
 
-std::string detokenize(const std::vector<std::string>& tokens){
-    std::string sum;
-    for(auto& token : tokens)
-        sum += token + " ";
-    sum.pop_back();
-    return sum;
-}
+std::string detokenize(const std::vector<std::string>& tokens);
+
+void sigpipe_handler(int signum);
