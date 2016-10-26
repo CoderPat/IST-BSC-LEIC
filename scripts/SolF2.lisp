175afa88ae77e3429274ad2c0d3421322aa79252
CoderPat <pattuga@gmail.com>
2016-10-26 21:56:59 +0100

    Add draft for the second delivery

---
+++ scripts/SolF2.lisp
@@ -0,0 +1,140 @@
+(load "datastructures.lisp")
+(load "auxfuncs.lisp")
+
+(defvar *count*)
+(setf *count* 0)
+
+;;; TAI position
+(defun make-pos (c l)
+    (list c l))
+(defun pos-l (pos)
+    (first pos))
+(defun pos-c (pos)
+    (second pos))
+
+;;; TAI acceleration
+(defun make-acce (c l)
+    (list c l))
+(defun acce-l (pos)
+    (first pos))
+(defun acce-c (pos)
+    (second pos))
+
+;;; TAI velocity
+(defun make-vel (c l)
+    (list c l))
+(defun vel-l (pos)
+    (first pos))
+(defun vel-c (pos)
+    (second pos))
+
+
+;; Solution of phase 1
+
+(defun getTrackContent (pos track)
+    (nth (pos-c pos) (nth (pos-l pos) (track-env track))))
+
+;; Pedir 0,4
+(defun isObstaclep (pos track)
+    "check if the position pos is an obstacle"
+    (or (< (pos-l pos) 0) (< (pos-c pos) 0)
+            (>= (pos-l pos) (pos-l (track-size track)))
+            (>= (pos-c pos) (pos-c (track-size track)))
+            (null (getTrackContent pos track))))
+
+;; Pedir 0,4
+(defun isGoalp (st) 
+    "check if st is a solution of the problem"
+    (let ((current-position (state-pos st))
+    (track (state-track st)))
+        (and (member current-position (track-endpositions track) :test #'equalp)
+     T)))
+
+;; Pedir 1,2
+(defun nextState (st act)
+    "generate the nextState after state st and action act from prolem"
+    (let ((new-state (make-state :action act :track (state-track st))))
+        (setf (state-vel new-state)
+        (make-vel (+ (vel-l (state-vel st)) (acce-l act))
+                (+ (vel-c (state-vel st)) (acce-c act))))
+        (setf (state-pos new-state)
+        (make-pos (+ (pos-l (state-pos st)) (vel-l (state-vel new-state)))
+                (+ (pos-c (state-pos st)) (vel-c (state-vel new-state)))))
+        (setf (state-cost new-state)
+        (cond ((isGoalp new-state) -100)
+        ((isObstaclep (state-pos new-state) (state-track new-state)) 20)
+        (T 1)))
+        (when (= (state-cost new-state) 20)
+            (setf (state-vel new-state) (make-vel 0 0))
+            (setf (state-pos new-state) (make-pos (pos-l (state-pos st))
+                            (pos-c (state-pos st)))))
+        (values new-state)))
+
+
+
+;; Solution of phase 2
+
+;;; Pedir 
+
+(defun nextStates (st)
+  "Returns a list of all the possible states next to a given state"
+  (list (nextState st '(-1 -1))
+        (nextState st '(1 1))
+        (nextState st '(-1 1))
+        (nextState st '(1 -1))
+        (nextState st '(0 -1))
+        (nextState st '(-1 0))
+        (nextState st '(1 0))
+        (nextState st '(0 1))
+        (nextState st '(0 0)))
+)
+
+
+(defun ldfs-aux (curr_node curr_d lim)
+    (if (> curr_d lim) (return-from ldfs-aux nil))
+    (if (isGoalp (node-state curr_node)) (return-from ldfs-aux (list curr_node)))
+    (let ( (path nil)
+           (next_states (nextStates (node-state curr_node))))
+         (print *count*)
+         (incf *count*)
+         (dolist (next_state next_states)
+             (setf child_ret (ldfs-aux (make-node :parent curr_node
+                                                  :state next_state
+                                                  :g (+ (state-cost next_state) (node-g curr_node)))
+                                       (1+ curr_d) lim))
+             (if (not (null child_ret)) (return (cons curr_node child_ret)))
+             null)
+    )
+)
+
+;;; limdepthfirstsearch 
+(defun limdepthfirstsearch (problem lim &key cutoff?)
+    "limited depth first search
+         st - initial state
+         problem - problem information
+         lim - depth limit"
+    (ldfs-aux (make-node :state (problem-initial-state problem)
+                         :g 0
+                         :parent nil) 
+              0 lim)
+)
+                            
+
+;iterlimdepthfirstsearch
+(defun iterlimdepthfirstsearch (problem &key (lim most-positive-fixnum))
+    "limited depth first search
+         st - initial state
+         problem - problem information
+         lim - limit of depth iterations"
+    (list (make-node :state (problem-initial-state problem))) )
+
+(print "here")
+(defvar non-goal-state)
+(setf non-goal-state 
+  (make-STATE :POS '(3 6)
+          :VEL '(-1 2)
+          :ACTION '(-1 0)
+          :COST 1
+          :TRACK *track*
+          :OTHER NIL))
+(length (ldfs-aux (make-node :state non-goal-state :g 0 :parent nil) 0 4))
