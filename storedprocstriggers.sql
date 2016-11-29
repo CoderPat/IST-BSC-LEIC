f742fa584fbe93ef58b1e6cc8f5a7e56544e2ef5
Patrick Fernandes <pattuga@gmail.com>
2016-11-29 23:55:34 +0000

    change database structure

--- storedprocstriggers.sql
+++ storedprocstriggers.sql
@@ -3,21 +3,21 @@ DROP TRIGGER IF EXISTS pagar_reserva;
 
 DELIMITER $$
 
-/*CREATE TRIGGER inserir_oferta BEFORE INSERT 
-ON Oferta
+CREATE TRIGGER inserir_oferta BEFORE INSERT 
+ON oferta
 FOR EACH ROW
 BEGIN
-	IF EXISTS ( SELECT 1 FROM Oferta WHERE (NEW.data_fim >= data_inicio) AND (data_fim >= NEW.data_inicio) )
+	IF EXISTS ( SELECT 1 FROM oferta WHERE (NEW.data_fim >= data_inicio) AND (data_fim >= NEW.data_inicio) )
 	THEN
 		SIGNAL SQLSTATE	'45000';
 	END IF;
-END$$ */
+END$$ 
 
 CREATE TRIGGER pagar_reserva BEFORE INSERT
-ON Paga
+ON paga
 FOR EACH ROW
 BEGIN
-	IF ( SELECT MAX(date(timestamp)) FROM Estado WHERE numero=NEW.numero ) > NEW.data
+	IF ( SELECT MAX(date(timestamp)) FROM estado WHERE numero=NEW.numero ) > NEW.data
 	THEN
 		SIGNAL SQLSTATE	'45000';
 	END IF;
