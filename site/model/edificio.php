35079feebf0835cda443bdd57f06118f4895e052
Patrick Santos Fernandes <ist181191@sigma02.ist.utl.pt>
2016-12-01 17:04:59 +0000

    Add total per space page

--- site/model/edificio.php
+++ site/model/edificio.php
@@ -4,7 +4,28 @@
 //por uma questao de simplicidade, acho que faz mais sentido lidarmos com
 //atualizacoes a db dentro do proprio endpoint na api (porque fica mais facil de ler o codigo)
 function edificio_getall($db) {
-	$QUERY = "SELECT * FROM edificio";
-    return $db->query($QUERY);
+	$query = "SELECT * FROM edificio";
+ 	return $db->query($query);
+}
+
+function edificio_get_alugaveis_total($db, $morada){
+	$query_model = "SELECT morada, codigo, SUM(custo) as total
+			FROM 
+			(
+				SELECT morada, codigo, tarifa*DATEDIFF(data_fim,data_inicio) as custo
+				FROM espaco NATURAL JOIN oferta NATURAL JOIN aluga NATURAL JOIN paga
+				WHERE YEAR(data) = '2016'
+				UNION
+				SELECT morada, codigo_espaco AS codigo, tarifa*DATEDIFF(data_fim,data_inicio) as custo
+ 				FROM posto NATURAL JOIN oferta NATURAL JOIN aluga NATURAL JOIN paga
+ 				WHERE YEAR(data) = '2016'
+			) as all_custos
+			WHERE morada=\"Catolica\"
+			GROUP BY morada, codigo;";
+	return $db->query($query_model);
+	//$prepared_query = $db->prepare($query_model);
+	//$prepared_query->bindParam(":morada", $morada);
+	//$prepared_query->execute();
+	//return $prepared_query;
 }
 ?>
--- site/pages/edificio/view.php
+++ site/pages/edificio/view.php
@@ -4,30 +4,24 @@ $title = "Ver edifício";
 include $root . "template/head.php"; 
 include $root . "template/navbar.php";
 
+include $root . "model/edificio.php";
+include $root . "view/table.php";
+
 if (isset($_GET['morada']) && !empty($_GET['morada'])) {
-    $morada = int($_GET['morada']);
+    $morada = $_GET['morada'];
 } else {
     $morada = 0;
 }
 
-$edificio = edificio_get($morada); //Isto retorna um objeto de edificio ja sem estar incorporado em PDO
-//depois usa-se diretamente aqui o $edificio para escrever o que for preciso
-$alugaveis = alugavel_getall_by_morada($morada);
+$edificio_total = edificio_get_alugaveis_total($db, $morada); 
 ?>
 
-
-<?
-draw_table($alugaveis);
-//TODO: por aqui todas as outras tables que têm  de ser desenhadas para isto
-
-//Faz sentido a funcao para criar espacos estar aqui e nao em nenhum outro lado
-
-//Incluir aqui tambem a lista de ofertas sobre este espaco.
-
-//E a lista de reservas
+<div class="container">
+<br>
+<?php
+draw_table($edificio_total, "Total por Espaco em $morada", null, ["Codigo", "Total"], [null, null], [["codigo"], ["total"]], null);
 ?>
-
-
+</div>
 
 <?php
 include $root . "template/foot.php";
