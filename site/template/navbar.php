89cefe87c9e81d4baef3df626d8f580de232059a
Afonso Tinoco <afonso.tfaria21@gmail.com>
2016-12-02 14:51:40 +0000

    UX scrubs scrubing again

--- site/template/navbar.php
+++ site/template/navbar.php
@@ -11,8 +11,24 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#"><?= $title ?></a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?= $title ?> <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-header">Edifícios</li>
+                        <li><a href="<?=$webroot?>/pages/edificio">Ver Edifícios</a></li>
+                        <li role="separator" class="divider">Alugáveis</li>
+                        <li class="dropdown-header">Alugáveis</li>
+                        <li><a href="<?=$webroot?>/pages/espaco">Ver Espaços</a></li>
+                        <li><a href="<?=$webroot?>/pages/posto">Ver Postos</a></li>
+                        <li role="separator" class="divider">Ofertas</li>
+                        <li class="dropdown-header">Ofertas</li>
+                        <li><a href="<?=$webroot?>/pages/oferta">Ver Ofertas</a></li>
+                        <li><a href="<?=$webroot?>/pages/reserva">Ver Reservas</a></li>
+                    </ul>
+                </li>
+                
             </ul>
+            
         </div><!--/.navbar-collapse -->
     </div>
 </nav>
