e944ff5b7605c12f04c06935be28ddaced207d2b
Patrick Santos Fernandes <ist181191@lab10p6.rnl.tecnico.ulisboa.pt>
2016-12-14 09:26:33 +0000

    Add measure table

--- warehouse-creation.sql
+++ warehouse-creation.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS wh_informacao;
 
 --necessary user_id in this case?
 CREATE TABLE wh_dim_user(
-	user_id int NOT NULL AUTO_INCREMENT,
+	user_id int unsigned NOT NULL AUTO_INCREMENT,
     nif varchar(9) NOT NULL unique,
     nome varchar(80) NOT NULL,
     telefone varchar(26) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE wh_dim_user(
 )
 
 CREATE TABLE wh_dim_data(
-	data_id int NOT NULL AUTO_INCREMENT,
+	data_id int unsigned  NOT NULL AUTO_INCREMENT,
     dia tinyint unsigned NOT NULL,
     semana tinyint unsigned NOT NULL
     mes tinyint unsigned NOT NULL,
@@ -25,8 +25,8 @@ CREATE TABLE wh_dim_data(
 
 CREATE TABLE wh_dimensao_tempo(
     tempo_id int unsigned NOT NULL AUTO_INCREMENT,
-    hour tinyint unsigned NOT NULL,
-    day tinyint unsigned NOT NULL,
+    hora tinyint unsigned NOT NULL,
+    minuto tinyint unsigned NOT NULL,
 	PRIMARY KEY (time_id)
 )
 
@@ -38,14 +38,26 @@ CREATE TABLE wh_dim_localizacao(
 	PRIMARY KEY (local_id)
 )
 
+CREATE TABLE wh_informacao(
+	user_id int unsigned NOT NULL,
+	data_id int unsigned NOT NULL,
+	tempo_id int unsigned NOT NULL,
+	local_id int unsigned NOT NULL,
+	montante int NOT NULL,
+	duracao int NOT NULL,
+	PRIMARY KEY (user_id, data_id, tempo_id, local_id ),
+	FOREIGN KEY (user_id) REFERENCES REFERENCES wh_dim_user(user_id),
+	FOREIGN KEY (data_id) REFERENCES REFERENCES wh_dim_data(data_id),
+	FOREIGN KEY (tempo_id) REFERENCES REFERENCES wh_dim_tempo(tempo_id),
+	FOREIGN KEY (local_id) REFERENCES REFERENCES wh_dim_localizacao(local_id),
+)
+
 --only paid reservations
 DELIMITER $$
 
 CREATE PROCEDURE carregar_dim_datas(ano) 
 BEGIN
 START TRANSACTION
-	DROP TABLE IF EXISTS wh_dim_data;
-
 	DECLARE curr_date date;
 	SET curr_date = DATE(ano + '-01-01');
 	WHILE (YEAR(curr_date)!=ano+1) DO
@@ -59,12 +71,10 @@ END$$
 CREATE PROCEDURE carregar_dim_tempo() 
 BEGIN
 START TRANSACTION
-	DROP TABLE IF EXISTS wh_dim_tempo;
-
 	DECLARE curr_ts timestamp;
 	SET curr_ts = timestamp('0000-01-01 00:00:00'); 
 	WHILE (DAY(curr_ts)!=2) DO
-		INSERT INTO wh_dim_tempo (hora, segundo) VALUES (HOUR(curr_ts), SECOND(curr_ts)); -- IS THE HIERARCHY STRICT? (second only between 1-60?)
+		INSERT INTO wh_dim_tempo (hora, segundo) VALUES (HOUR(curr_ts), MINUTE(curr_ts)); -- IS THE HIERARCHY STRICT? (second only between 1-60?)
 		SET curr_ts = DATE_ADD(curr_ts, INTERVAL 1 SECOND);
 	END WHILE;
 COMMIT;
@@ -74,30 +84,16 @@ END$$
 CREATE PROCEDURE carregar_dim_user()
 BEGIN
 START TRANSACTION
-	DROP TABLE IF EXISTS wh_dim_user;
-
 	INSERT INTO wh_dim_user (nif, nome, telefone) 
 		SELECT *
 		FROM User;
 COMMIT;
 END$$
 
-CREATE PROCEDURE carregar_dim_user()
-BEGIN
-START TRANSACTION
-	DROP TABLE IF EXISTS wh_dim_user;
-
-	INSERT INTO wh_dim_user (nif, nome, telefone) 
-		SELECT nif, nome, telefone
-		FROM User;
-COMMIT;
-END$$
 
 CREATE PROCEDURE carregar_dim_localizacao()
 BEGIN
 START TRANSACTION
-	DROP TABLE IF EXISTS wh_dim_localizacao;
-
 	INSERT INTO wh_dim_localizacao (morada, codigo_espaco, codigo_posto) 
 		SELECT morada, codigo, null
 		FROM Espaco
@@ -107,7 +103,25 @@ START TRANSACTION
 COMMIT;
 END$$
 
-DELIMITER ;
+CREATE PROCEDURE carregar_medidas()
+BEGIN
+START TRANSACTION
+	INSERT INTO wh_informacao (user_id, data_id, tempo_id, local_id) 
+		SELECT user_id, data_id, tempo_id, local_id, tarifa*(DATADIFF(data_fim,data_inicio)), DATADIFF(data_fim, data_inicio)
+		FROM paga NATURAL JOIN oferta INNER JOIN wh_dim_user U
+									  INNER JOIN wh_dim_data D
+									  INNER JOIN wh_dim_tempo T
+									  INNER JOIN wh_dim_localizacao L 
+		ON  U.nif = oferta.nif AND
+		   (D.dia = DAY(paga.data) AND D.mes = MONTH(paga.data) AND D.ano = YEAR(paga.data)) AND
+		   (T.hora = HOUR(paga.data) AND T.minuto = MINUTE(paga.data)) AND
+		   (L.morada = oferta.morada AND ((L.codigo_espaco = oferta.codigo AND L.codigo_post IS NULL) OR L.codigo_posto = oferta.codigo))
+COMMIT
+END$$
+
+DELIMITER;
+
+
 
 
 
