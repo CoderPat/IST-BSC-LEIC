3ee56623dc4351fb6e197138d4378f2fa4e82f8f
Afonso Tinoco <afonso.tfaria21@gmail.com>
2016-12-14 21:39:28 +0000

    wh works

--- warehouse-creation.sql
+++ warehouse-creation.sql
@@ -49,7 +49,8 @@ CREATE TABLE wh_informacao(
 	data_id int unsigned NOT NULL,
 	tempo_id int unsigned NOT NULL,
 	local_id int unsigned NOT NULL,
-	montante int NOT NULL, 
+	montante int NOT NULL,
+	duracao int NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES wh_dim_user(user_id),	
 	FOREIGN KEY (data_id) REFERENCES wh_dim_data(data_id),	
 	FOREIGN KEY (tempo_id) REFERENCES wh_dim_tempo(tempo_id),
@@ -64,7 +65,7 @@ BEGIN
 	DECLARE curr_date datetime;
 	SET curr_date = '2016-01-01 00:00:00';
 	WHILE (YEAR(curr_date)<>2018) DO
-		INSERT INTO wh_dim_data (dia, semana, mes, semestre, ano) VALUES (DAY(curr_date), 1, MONTH(curr_date),1, ano); #IS THE HIERARCHY STRICT? (MONTH RANGE ONLY 1-6, WEEK RANGE ONLY 1-5?)
+		INSERT INTO wh_dim_data (dia, semana, mes, semestre, ano) VALUES (DAY(curr_date), WEEK(curr_date, 4), MONTH(curr_date), (QUARTER(curr_date)+1) DIV 2, YEAR(curr_date)); #IS THE HIERARCHY STRICT? (MONTH RANGE ONLY 1-6, WEEK RANGE ONLY 1-5?)
 		SET curr_date = DATE_ADD(curr_date, INTERVAL 1 DAY);
 	END WHILE;
 END$$
@@ -73,10 +74,10 @@ END$$
 CREATE PROCEDURE carregar_dim_tempo() 
 BEGIN
 	DECLARE curr_ts datetime;
-	SET curr_ts = '0000-01-01 00:00:00'; 
+	SET curr_ts = '1971-01-01 00:00:00'; 
 	WHILE (DAY(curr_ts)!=2) DO
 		INSERT INTO wh_dim_tempo (hora, minuto) VALUES (HOUR(curr_ts), MINUTE(curr_ts)); #IS THE HIERARCHY STRICT? (second only between 1-60?)
-		SET curr_ts = DATE_ADD(curr_ts, INTERVAL 1 SECOND);
+		SET curr_ts = DATE_ADD(curr_ts, INTERVAL 1 MINUTE);
 	END WHILE;
 END$$
 
@@ -101,16 +102,16 @@ END$$
 
 CREATE PROCEDURE carregar_medidas()
 BEGIN
-	INSERT INTO wh_informacao (user_id, data_id, tempo_id, local_id) 
+	INSERT INTO wh_informacao (user_id, data_id, tempo_id, local_id, montante, duracao) 
 		SELECT user_id, data_id, tempo_id, local_id, tarifa*(DATEDIFF(data_fim,data_inicio)), DATEDIFF(data_fim, data_inicio)
-		FROM paga NATURAL JOIN oferta INNER JOIN wh_dim_user U
-									  INNER JOIN wh_dim_data D
-									  INNER JOIN wh_dim_tempo T
-									  INNER JOIN wh_dim_localizacao L 
-		ON  U.nif = oferta.nif AND
+		FROM (paga NATURAL JOIN oferta NATURAL JOIN aluga) INNER JOIN wh_dim_user U
+									  					   INNER JOIN wh_dim_data D
+									  					   INNER JOIN wh_dim_tempo T
+									  					   INNER JOIN wh_dim_localizacao L 
+		ON  U.nif = aluga.nif AND
 		   (D.dia = DAY(paga.data) AND D.mes = MONTH(paga.data) AND D.ano = YEAR(paga.data)) AND
 		   (T.hora = HOUR(paga.data) AND T.minuto = MINUTE(paga.data)) AND
-		   (L.morada = oferta.morada AND ((L.codigo_espaco = oferta.codigo AND L.codigo_post IS NULL) OR L.codigo_posto = oferta.codigo));
+		   (L.morada = oferta.morada AND ((L.codigo_espaco = oferta.codigo AND L.codigo_posto IS NULL) OR L.codigo_posto = oferta.codigo));
 END$$
 
 DELIMITER ;
@@ -122,5 +123,5 @@ call carregar_dim_user();
 call carregar_medidas();
 
 SELECT dia, semana, mes, semestre, ano, morada, codigo_espaco, codigo_posto, AVG(montante) as media
-from wh_informacao NATURAL JOIN wh_dim_localizacao NATURAL JOIN wh_dim_data  
-group by dia, semana, mes, semestre, ano, morada, codigo_espaco, codigo_posto with rollup;
+from wh_informacao NATURAL JOIN wh_dim_localizacao NATURAL JOIN wh_dim_data
+group by dia, mes, codigo_espaco, codigo_posto with rollup;
