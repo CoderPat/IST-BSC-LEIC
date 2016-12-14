84a46b544f7a8c7ed3a27b0732bce14891137f56
Afonso Tinoco <afonso.tfaria21@gmail.com>
2016-12-14 20:30:27 +0000

    Fixed most creation queires

--- warehouse-creation.sql
+++ warehouse-creation.sql
@@ -1,8 +1,14 @@
+DROP TABLE IF EXISTS wh_informacao;
 DROP TABLE IF EXISTS wh_dim_user;
 DROP TABLE IF EXISTS wh_dim_data;
 DROP TABLE IF EXISTS wh_dim_tempo;
 DROP TABLE IF EXISTS wh_dim_localizacao;
-DROP TABLE IF EXISTS wh_informacao;
+DROP PROCEDURE IF EXISTS carregar_medidas;
+DROP PROCEDURE IF EXISTS carregar_dim_datas;
+DROP PROCEDURE IF EXISTS carregar_dim_localizacao;
+DROP PROCEDURE IF EXISTS carregar_dim_tempo;
+DROP PROCEDURE IF EXISTS carregar_dim_user;
+
 
 #necessary user_id in this case?
 CREATE TABLE wh_dim_user(
@@ -50,60 +56,51 @@ CREATE TABLE wh_informacao(
 	FOREIGN KEY (local_id) REFERENCES wh_dim_localizacao(local_id)
 );
 
---only paid reservations
+#only paid reservations
 DELIMITER $$
 
-CREATE PROCEDURE carregar_dim_datas(ano) 
+CREATE PROCEDURE carregar_dim_datas() 
 BEGIN
-START TRANSACTION
-	DECLARE curr_date date;
-	SET curr_date = DATE(ano + '-01-01');
-	WHILE (YEAR(curr_date)!=ano+1) DO
-		INSERT INTO wh_dim_data (dia, semana, mes, semestre, ano) VALUES (DAY(curr_date), WEEK(curr_date), MONTH(curr_date), SEMESTER(curr_date), ano); -- IS THE HIERARCHY STRICT? (MONTH RANGE ONLY 1-6, WEEK RANGE ONLY 1-5?)
+	DECLARE curr_date datetime;
+	SET curr_date = '2016-01-01 00:00:00';
+	WHILE (YEAR(curr_date)<>2018) DO
+		INSERT INTO wh_dim_data (dia, semana, mes, semestre, ano) VALUES (DAY(curr_date), WEEK(curr_date), MONTH(curr_date), SEMESTER(curr_date), ano); #IS THE HIERARCHY STRICT? (MONTH RANGE ONLY 1-6, WEEK RANGE ONLY 1-5?)
 		SET curr_date = DATE_ADD(curr_date, INTERVAL 1 DAY);
 	END WHILE;
-COMMIT;
 END$$
 
 
 CREATE PROCEDURE carregar_dim_tempo() 
 BEGIN
-START TRANSACTION
-	DECLARE curr_ts timestamp;
-	SET curr_ts = timestamp('0000-01-01 00:00:00'); 
+	DECLARE curr_ts datetime;
+	SET curr_ts = '0000-01-01 00:00:00'; 
 	WHILE (DAY(curr_ts)!=2) DO
-		INSERT INTO wh_dim_tempo (hora, segundo) VALUES (HOUR(curr_ts), MINUTE(curr_ts)); -- IS THE HIERARCHY STRICT? (second only between 1-60?)
+		INSERT INTO wh_dim_tempo (hora, segundo) VALUES (HOUR(curr_ts), MINUTE(curr_ts)); #IS THE HIERARCHY STRICT? (second only between 1-60?)
 		SET curr_ts = DATE_ADD(curr_ts, INTERVAL 1 SECOND);
 	END WHILE;
-COMMIT;
 END$$
 
 
 CREATE PROCEDURE carregar_dim_user()
 BEGIN
-START TRANSACTION
 	INSERT INTO wh_dim_user (nif, nome, telefone) 
 		SELECT *
-		FROM User;
-COMMIT;
+		FROM user;
 END$$
 
 
 CREATE PROCEDURE carregar_dim_localizacao()
 BEGIN
-START TRANSACTION
 	INSERT INTO wh_dim_localizacao (morada, codigo_espaco, codigo_posto) 
 		SELECT morada, codigo, null
-		FROM Espaco
+		FROM espaco
 		UNION
 		SELECT morada, codigo_espaco, codigo
-		FROM Posto
-COMMIT;
+		FROM posto;
 END$$
 
 CREATE PROCEDURE carregar_medidas()
 BEGIN
-START TRANSACTION
 	INSERT INTO wh_informacao (user_id, data_id, tempo_id, local_id) 
 		SELECT user_id, data_id, tempo_id, local_id, tarifa*(DATADIFF(data_fim,data_inicio)), DATADIFF(data_fim, data_inicio)
 		FROM paga NATURAL JOIN oferta INNER JOIN wh_dim_user U
@@ -114,7 +111,6 @@ START TRANSACTION
 		   (D.dia = DAY(paga.data) AND D.mes = MONTH(paga.data) AND D.ano = YEAR(paga.data)) AND
 		   (T.hora = HOUR(paga.data) AND T.minuto = MINUTE(paga.data)) AND
 		   (L.morada = oferta.morada AND ((L.codigo_espaco = oferta.codigo AND L.codigo_post IS NULL) OR L.codigo_posto = oferta.codigo))
-COMMIT
 END$$
 
 DELIMITER;
